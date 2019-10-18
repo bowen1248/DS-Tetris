8ea2330531a0e9878174d2d020bdd07ab8ac20de
coder-1248 <bowenlee31@gmail.com>
2019-10-18 22:05:28 +0800

    test case ok

--- main.cpp
+++ main.cpp
@@ -3,24 +3,28 @@
 #include <string>
 using namespace std;
 
+// generate block
 class block {
-    friend class Gameboard;
+    friend class Gameboard; // get access to board rows and cols
     private:
-        int shape[4][2];
+        int shape[4][2];    // shape[block index][0: x coord. 1: y coord.]
     public:
+        // get shape of the block (x,y coordinate of 4 blocks)
         void setblock(int in_shape[], int col, int rows) {
             int ini_col = col - 1;
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 4; i++) {   // set initial 4 block coordinate
                 shape[i][0] = (in_shape[i] % 4) + ini_col;
                 shape[i][1] = (in_shape[i] / 4) + rows;
             }
         }
+        // make block fall 1 row
         void block_drop() {
             for (int i = 0; i < 4; i++) 
                 shape[i][1]--;
         }
 };
 
+// overall tetris process and management
 class Gameboard {
     private:
         int rows, cols;
@@ -32,23 +36,23 @@ class Gameboard {
             board = new int*[rows + 4];
             for (int i = 0; i < rows + 4; i++)
                 board[i] = new int[cols];
-            for (int j = 0; j < rows; j++) {
+            for (int j = 0; j < rows + 4; j++) {
                 for (int k = 0; k < cols; k++)
                     board[j][k] = 0;
             }
         }
-
+        // to test if block crash after block drop a tile 
         bool clash(block a) {
             for (int i = 0; i < 4; i++) {
-                if ((a.shape[i][1] - 1) < 0) return false; 
-                else if (board[(a.shape[i][1] - 1)] [a.shape[i][0]] == 1) return false;
+                if ((a.shape[i][1] - 1) < 0) return true; 
+                else if (board[(a.shape[i][1] - 1)] [a.shape[i][0]] == 1) return true;
             }
-            return true;
+            return false;
         }
-
+        // check if board formed a line and drop all block above it
         bool check_line () {
             bool disappear = false;
-            for (int i = 0; i < rows; i++) {
+            for (int i = 0; i < rows + 4; i++) {
                 bool form_line = true;
                 if (disappear) {
                     form_line = true;
@@ -63,50 +67,58 @@ class Gameboard {
             }
             return disappear;
         }
+        // process block things
         bool block_fall(block a) {
-            int gameover = 0;
-            while (clash(a)) {
+            while (!clash(a)) {    // drop block until clash with others
                 a.block_drop();
             }
+            // record the block position
             for (int i = 0; i < 4; i++) 
                 board [a.shape[i][1]] [a.shape[i][0]] = 1;
-            for (int i = 0; i < 4; i++)
-                if (a.shape[i][1] >= rows) return 1;
+            // process if line is formed
             while (check_line()) {}
-            return 0;
+            // see if the block has stacked over border vertical height
+            for (int i = 0; i < cols; i++)
+                if(board[rows][i] == 1) return 1;   // terminate this game
+            
+            return 0;   // continue game
         }
 
+        // print out matrix
         void print() {
             for (int j = (rows - 1); j >= 0; j--) {
                 for (int k = 0; k < cols; k++)
-                    cout << board[j][k] << " ";
+                    cout << board[j][k];
                 cout << endl;
             }
         }
 };
 
 int main (void) {
-    #ifdef DEBUG
-    freopen("tetris.data.txt", "r", stdin);
+    #ifdef DEBUG    // open files
+    freopen("Tetris.data", "r", stdin);
+    freopen("Tetris.output", "w", stdout);
     #endif
-    int rows, cols;
-    int block_col;
-    int shape[4];
-    int shape_menu[19][4] = {
+    int rows, cols; // board total cols and rows
+    int block_col;  // store block initial column
+    int shape[4];   // 
+    int shape_menu[19][4] = {   // record all block shape
         {1,4,5,6}, {1,4,5,9}, {0,1,2,5}, {0,4,5,8}, {0,1,4,8},
         {0,4,5,6}, {1,5,8,9}, {0,1,2,6}, {0,1,5,9}, {0,1,2,4},
         {0,4,8,9}, {2,4,5,6}, {0,1,5,6}, {1,4,5,8}, {1,2,4,5},
         {0,4,5,9}, {0,4,8,12}, {0,1,2,3}, {0,1,4,5}
     };
-    string instring;
+    string instring;    // get cols and rows
     cin >> rows >> cols;
-    Gameboard mainboard {rows,cols};
-    block b1;
+    Gameboard mainboard {rows,cols};    // initialize matrix
+    block b1;   // generate a block 
     bool gameover = false;
+    // the loop continues until code terminate or gameover(block vertical > board rows)
     for (cin >> instring; instring.compare("End") && !gameover; cin >> instring) {
         int sel_shape;
         int ini_col;
-            switch (instring[0]) {
+        // judge the block shape by instring
+        switch (instring[0]) {
             case 'T': {if (instring[1] == '1') sel_shape = 0;
                         else if (instring[1] == '2') sel_shape = 1;
                         else if (instring[1] == '3') sel_shape = 2;
@@ -136,12 +148,15 @@ int main (void) {
             case 'O': sel_shape = 18; break;
             default: sel_shape = 0; break;
         }
+        // put shape into shape initializer
         for (int i = 0; i < 4; i++)
             shape[i] = shape_menu[sel_shape][i];
         cin >> ini_col;
         b1.setblock(shape, ini_col, rows);
+        // drop the block
         gameover = mainboard.block_fall(b1);
 }
+    // print last result
     mainboard.print();
     return 0;
 }
